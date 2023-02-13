3d602da0cb47e72eaaea087d5580c7a6b0c74810
aladd2021 <aladd@ltu.edu>
2023-02-13 15:06:21 -0500

    HW3 for CS UR1
    hw3

---
+++ HelloEmgu/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace HelloEmgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.emguPictureBox = new System.Windows.Forms.PictureBox();
+            this.binaryPictureBox = new System.Windows.Forms.PictureBox();
+            this.thresholdTrackbar = new System.Windows.Forms.TrackBar();
+            this.whitePixelsLFLbl = new System.Windows.Forms.Label();
+            this.whitePixelsLLbl = new System.Windows.Forms.Label();
+            this.whitePixelsMLbl = new System.Windows.Forms.Label();
+            this.whitePixelsRLbl = new System.Windows.Forms.Label();
+            this.whitePixelsRFLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackbar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // emguPictureBox
+            // 
+            this.emguPictureBox.Location = new System.Drawing.Point(37, 47);
+            this.emguPictureBox.Name = "emguPictureBox";
+            this.emguPictureBox.Size = new System.Drawing.Size(342, 267);
+            this.emguPictureBox.TabIndex = 0;
+            this.emguPictureBox.TabStop = false;
+            // 
+            // binaryPictureBox
+            // 
+            this.binaryPictureBox.Location = new System.Drawing.Point(414, 47);
+            this.binaryPictureBox.Name = "binaryPictureBox";
+            this.binaryPictureBox.Size = new System.Drawing.Size(342, 267);
+            this.binaryPictureBox.TabIndex = 1;
+            this.binaryPictureBox.TabStop = false;
+            // 
+            // thresholdTrackbar
+            // 
+            this.thresholdTrackbar.Location = new System.Drawing.Point(37, 349);
+            this.thresholdTrackbar.Maximum = 255;
+            this.thresholdTrackbar.Minimum = 1;
+            this.thresholdTrackbar.Name = "thresholdTrackbar";
+            this.thresholdTrackbar.Size = new System.Drawing.Size(719, 69);
+            this.thresholdTrackbar.TabIndex = 2;
+            this.thresholdTrackbar.Value = 1;
+            this.thresholdTrackbar.ValueChanged += new System.EventHandler(this.thresholdTrackbar_ValueChanged);
+            // 
+            // whitePixelsLFLbl
+            // 
+            this.whitePixelsLFLbl.AutoSize = true;
+            this.whitePixelsLFLbl.Location = new System.Drawing.Point(12, 9);
+            this.whitePixelsLFLbl.Name = "whitePixelsLFLbl";
+            this.whitePixelsLFLbl.Size = new System.Drawing.Size(94, 20);
+            this.whitePixelsLFLbl.TabIndex = 3;
+            this.whitePixelsLFLbl.Text = "White Pixels";
+            // 
+            // whitePixelsLLbl
+            // 
+            this.whitePixelsLLbl.AutoSize = true;
+            this.whitePixelsLLbl.Location = new System.Drawing.Point(190, 9);
+            this.whitePixelsLLbl.Name = "whitePixelsLLbl";
+            this.whitePixelsLLbl.Size = new System.Drawing.Size(94, 20);
+            this.whitePixelsLLbl.TabIndex = 4;
+            this.whitePixelsLLbl.Text = "White Pixels";
+            // 
+            // whitePixelsMLbl
+            // 
+            this.whitePixelsMLbl.AutoSize = true;
+            this.whitePixelsMLbl.Location = new System.Drawing.Point(349, 9);
+            this.whitePixelsMLbl.Name = "whitePixelsMLbl";
+            this.whitePixelsMLbl.Size = new System.Drawing.Size(94, 20);
+            this.whitePixelsMLbl.TabIndex = 5;
+            this.whitePixelsMLbl.Text = "White Pixels";
+            // 
+            // whitePixelsRLbl
+            // 
+            this.whitePixelsRLbl.AutoSize = true;
+            this.whitePixelsRLbl.Location = new System.Drawing.Point(522, 9);
+            this.whitePixelsRLbl.Name = "whitePixelsRLbl";
+            this.whitePixelsRLbl.Size = new System.Drawing.Size(94, 20);
+            this.whitePixelsRLbl.TabIndex = 6;
+            this.whitePixelsRLbl.Text = "White Pixels";
+            // 
+            // whitePixelsRFLbl
+            // 
+            this.whitePixelsRFLbl.AutoSize = true;
+            this.whitePixelsRFLbl.Location = new System.Drawing.Point(683, 9);
+            this.whitePixelsRFLbl.Name = "whitePixelsRFLbl";
+            this.whitePixelsRFLbl.Size = new System.Drawing.Size(94, 20);
+            this.whitePixelsRFLbl.TabIndex = 7;
+            this.whitePixelsRFLbl.Text = "White Pixels";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.whitePixelsRFLbl);
+            this.Controls.Add(this.whitePixelsRLbl);
+            this.Controls.Add(this.whitePixelsMLbl);
+            this.Controls.Add(this.whitePixelsLLbl);
+            this.Controls.Add(this.whitePixelsLFLbl);
+            this.Controls.Add(this.thresholdTrackbar);
+            this.Controls.Add(this.binaryPictureBox);
+            this.Controls.Add(this.emguPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackbar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox emguPictureBox;
+        private System.Windows.Forms.PictureBox binaryPictureBox;
+        private System.Windows.Forms.TrackBar thresholdTrackbar;
+        private System.Windows.Forms.Label whitePixelsLFLbl;
+        private System.Windows.Forms.Label whitePixelsLLbl;
+        private System.Windows.Forms.Label whitePixelsMLbl;
+        private System.Windows.Forms.Label whitePixelsRLbl;
+        private System.Windows.Forms.Label whitePixelsRFLbl;
+    }
+}
+
---
+++ HelloEmgu/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloEmgu
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        private int _threshold = 150;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(1);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+            thresholdTrackbar.Value = _threshold;
+        }
+
+        private void DisplayWebcam()
+        {
+            while(_capture.IsOpened)
+            {
+                //frame maintnance
+                Mat frame = _capture.QueryFrame();
+
+                //Resize to Picturebox aspect ratio
+                int newHeight = (frame.Size.Height * emguPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(emguPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+
+                //display the image in box
+                emguPictureBox.Image = frame.ToBitmap();
+
+                Mat frameGrey = new Mat();
+                CvInvoke.CvtColor(frame, frameGrey, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(frameGrey, frameGrey, _threshold, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+                binaryPictureBox.Image = frameGrey.ToBitmap();
+
+                // White pixel count code
+                int whitePixelMostLeft = 0;
+                int whitePixelLeft = 0;
+                int whitePixelMiddle = 0;
+                int whitePixelRight = 0;
+                int whitePixelMostRight = 0;
+                Image<Gray, byte> img2 = frameGrey.ToImage<Gray, byte>();
+                //slice 1 - Most Left
+                for (int x = 0; x < frameGrey.Width / 5; x++)
+                {
+                    for (int y = 0; y < frameGrey.Height; y++)
+                    {
+                        if (img2.Data[y, x, 0] == 255) whitePixelMostLeft++;
+                    }
+                }
+
+                Invoke(new Action(() =>
+                {
+                    whitePixelsLFLbl.Text = $"{whitePixelMostLeft} White Pixels in FL";
+                }));
+                //Slice 1 - Most Left
+
+                // Slice 2 - Left
+                for (int Lx = frameGrey.Width / 5; Lx < (2 * frameGrey.Width) / 5; Lx++)
+                {
+                    for (int Ly = 0; Ly < frameGrey.Height; Ly++)
+                    {
+                        if (img2.Data[Ly, Lx, 0] == 255) whitePixelLeft++; 
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    whitePixelsLLbl.Text = $"{whitePixelLeft} White Pixels in L";
+                }));
+                // Slice 2 - Left
+
+                //Slice 3 - Middle
+                for (int Mx = (2 * frameGrey.Width) / 5; Mx < (3 * frameGrey.Width) / 5; Mx++)
+                {
+                    for (int My = 0; My < frameGrey.Height; My++)
+                    {
+                        if (img2.Data[My, Mx, 0] == 255) whitePixelMiddle++; 
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    whitePixelsMLbl.Text = $"{whitePixelMiddle} White Pixels in M";
+                }));
+                //Slice 3 - Middle
+
+                //slice 4 - Right
+                for (int Rx = (3 * frame.Width) / 5; Rx < (4 * frame.Width) / 5; Rx++)
+                {
+                    for (int Ry = 0; Ry < frame.Height; Ry++)
+                    {
+                        if (img2.Data[Ry, Rx, 0] == 255) whitePixelRight++;
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    whitePixelsRLbl.Text = $"{whitePixelRight} White Pixels in R";
+                }));
+                //Slice 4 - Right
+
+                //slice 5 - Far Right
+                for (int MRx = (4 * frameGrey.Width) / 5; MRx < frameGrey.Width; MRx++)
+                {
+                    for (int MRy = 0; MRy < frameGrey.Height; MRy++)
+                    {
+                        if (img2.Data[MRy, MRx, 0] == 255) whitePixelMostRight++; 
+                    }
+                }
+               
+                Invoke(new Action(() =>
+                {
+                    whitePixelsRFLbl.Text = $"{whitePixelMostRight} White Pixels in MR";
+                }));
+                //Slice 5 - Far Right
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // terminate the image processing thread to avoid orphaned processes
+            _captureThread.Abort();
+        }
+
+        private void thresholdTrackbar_ValueChanged(object sender, EventArgs e)
+        {
+            _threshold = thresholdTrackbar.Value;
+        }
+    }
+}
